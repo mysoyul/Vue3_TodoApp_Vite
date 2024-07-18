5a969b2e47ff65817f1f6499e36ad638ca2f7f48
mysoyul <vega2k_@naver.com>
2024-07-18 13:09:51 +0900

    TodoList TransitionGroup적용

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <ul>
+    <TransitionGroup name="list" tag="ul">
       <li v-for="(todo, index) in todoList" :key="index" class="shadow">
         <i class="fas fa-check checkBtn" :class="{ checkBtnCompleted: todo.completed }"
           @click="toggleComplete(todo, index)">
@@ -10,7 +10,7 @@
           <i class="fas fa-trash-alt"></i>
         </span>
       </li>
-    </ul>
+    </TransitionGroup>
   </div>
 </template>
 
@@ -64,5 +64,16 @@ li {
 .textCompleted {
   text-decoration: line-through;
   color: #b3adad;
+}
+
+.list-enter-active,
+.list-leave-active {
+  transition: all 0.5s ease;
+}
+
+.list-enter-from,
+.list-leave-to {
+  opacity: 0;
+  transform: translateX(30px);
 }
 </style>
