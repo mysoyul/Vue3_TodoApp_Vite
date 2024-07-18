3b254a5f031a7967e8a33e4733d43bdd5ff774d7
mysoyul <vega2k_@naver.com>
2024-07-18 17:14:49 +0900

    HelloWorld수정

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,12 @@
 <template>
   <div>
-    <TodoHeader></TodoHeader>
-    <TodoInput></TodoInput>
-    <TodoList></TodoList>
-    <TodoFooter></TodoFooter>
+
   </div>
 </template>
 
 <script>
-import TodoHeader from '@/components/TodoHeader.vue'
-import TodoInput from '@/components/TodoInput.vue'
-import TodoList from '@/components/TodoList.vue'
-import TodoFooter from '@/components/TodoFooter.vue'
-
 export default {
-  components: {
-    TodoHeader,
-    TodoInput,
-    TodoList,
-    TodoFooter
-  },
 
-  setup() {
-
-    return {}
-  } //setup
 }
 </script>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,25 @@
 <template>
-  <div></div>
+  <div>
+    <TodoHeader></TodoHeader>
+    <TodoInput></TodoInput>
+    <TodoList></TodoList>
+    <TodoFooter></TodoFooter>
+  </div>
 </template>
 
 <script>
+import TodoHeader from '@/components/TodoHeader.vue'
+import TodoInput from '@/components/TodoInput.vue'
+import TodoList from '@/components/TodoList.vue'
+import TodoFooter from '@/components/TodoFooter.vue'
+
 export default {
+  components: {
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
+  },
   setup() {
     return {}
   }
